c7dc193f946e80d397dcd94f236a5e5ba19c105e
Anna Gingle <48488699+AnnaGingle@users.noreply.github.com>
2019-09-10 21:49:33 -0500

    best

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
     const navlinks = document.querySelectorAll(".nav-links li");
-   
+
 
     burger.addEventListener("click", () => {
         //toggle nav
@@ -10,11 +10,11 @@ const navSlide = () => {
 
         //animate links
         navlinks.forEach((link, index) => {
-            if(link.style.animation) {
+            if (link.style.animation) {
                 link.style.animation = '';
-            } else{
-            link.style.animation = `navLinkFade 0.5s ease forward ${index / 7}s`;
-            }    
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forward ${index / 7}s`;
+            }
         });
         //burger animation
         burger.classList.toggle("toggle");
@@ -28,23 +28,22 @@ function parallax(element, distance, speed) {
     item.style.transform = `translateY(${distance * speed}px)`;
 }
 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     parallax("header", window.scrollY, 0.5);
     parallax("nav", window.scrollY, 1);
     parallax(".intro-text", window.scrollY, 0.6);
-    
+
     //parallax(".small-rose", window.scrollY, 0.4);
     //parallax(".big-rose", window.scrollY, 0.2);
 });
 //HANDSHAKE BUTTON
 window.open(
-    'https://app.joinhandshake.com/users/2744767',
     '_blank' // <- This is what makes it open in a new window.
-  );
+);
 
 navSlide();
 
 //const app = ()=>{
 //    navSlide();
-    
+
 //}
